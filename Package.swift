d12a240a1a10dc186b8555c0225fe9793e154110
LEE ZHE YU <tzef8220@gmail.com>
2022-06-19 11:49:03 +0800

    Add design system depenndence

--- Package.swift
+++ Package.swift
@@ -14,13 +14,23 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(
+            url: "https://github.com/tzef/ModularExample-DesignSystem.git",
+            exact: Version(1, 0, 0)
+        )
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ModuleUI",
-            dependencies: []),
+            dependencies: [
+                .product(
+                    name: "ModuleDesignSystem",
+                    package: "ModularExample-DesignSystem"
+                )
+            ]
+        ),
         .testTarget(
             name: "ModuleUITests",
             dependencies: ["ModuleUI"]),
--- Tests/ModuleUITests/ModuleUITests.swift
+++ Tests/ModuleUITests/ModuleUITests.swift
@@ -6,6 +6,6 @@ final class ModuleUITests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(ModuleUI().text, "Hello, World!")
+        // XCTAssertEqual(ModuleUI().text, "Hello, World!")
     }
 }
